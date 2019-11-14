7ce54eca1320ba99fc943e025b1ac24c516a5588
Sempolus <kuli25@wp.pl>
2019-11-14 19:42:37 +0100

    java_v2

---
+++ src/variables/variables.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class variables {
+    public static void main(String[] args) {
+        int age;
+        age = 14;
+        System.out.println(age);
+
+        double result = 0.01;
+        System.out.println("Wynik: " + result);
+    }
+}
